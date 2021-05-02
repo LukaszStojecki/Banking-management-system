4bf92f01413fc8b0554d66e9306395fd05198166
lukasz <lukasz.stojecki@o2.pl>
2021-05-02 15:42:26 +0200

    * added PasswordResetTokenRepository

---
+++ src/main/java/pl/stojecki/bankingmanagementsystem/user/repository/PasswordResetTokenRepository.java
@@ -0,0 +1,12 @@
+package pl.stojecki.bankingmanagementsystem.user.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pl.stojecki.bankingmanagementsystem.user.model.PasswordResetToken;
+
+import java.util.Optional;
+
+@Repository
+public interface PasswordResetTokenRepository extends JpaRepository<PasswordResetToken,Long> {
+    Optional<PasswordResetToken> findByToken(String token);
+}
--- src/main/java/pl/stojecki/bankingmanagementsystem/user/repository/ResetTokenRepository.java
+++
@@ -1,9 +0,0 @@
-package pl.stojecki.bankingmanagementsystem.user.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import pl.stojecki.bankingmanagementsystem.user.model.ResetToken;
-
-@Repository
-public interface ResetTokenRepository extends JpaRepository<ResetToken,Long> {
-}
