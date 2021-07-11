e86495f004bd613308115781ce8047b8babced2f
lukasz <lukasz.stojecki@o2.pl>
2021-07-11 20:51:06 +0200

    * updated reminder component

--- banking-management-system-frontend/src/app/auth/components/reminder/reminder.component.ts
+++ banking-management-system-frontend/src/app/auth/components/reminder/reminder.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {UserService} from "../../service/user.service";
+import {HttpErrorResponse} from "@angular/common/http";
+import {MatSnackBar} from "@angular/material/snack-bar";
 
 @Component({
   selector: 'app-reminder',
@@ -10,29 +12,34 @@ import {UserService} from "../../service/user.service";
 export class ReminderComponent implements OnInit {
 
   formGroup: FormGroup
-  isError: boolean;
 
   constructor(private userService: UserService,
-              private fb: FormBuilder) { }
+              private fb: FormBuilder,
+              private snackBar:MatSnackBar) { }
 
   ngOnInit(): void {
     this.formGroup = this.fb.group({
-      email: ['', [ Validators.required]]
+      email: ['', [ Validators.required,Validators.email]]
     });
   }
   get f() {
     return this.formGroup.controls;
   }
 
-  reminderIdentificationNumber() {
-    this.isError = false;
+  reminderIdentificationNumber(){
     this.userService.reminderIdentificationNumber(this.f.email.value)
-      .subscribe(res => {
-        console.log("check email")
-      }, error => {
-        console.log(error)
-        this.isError = true;
-      })
+      .subscribe(data => {
+      }, (error: HttpErrorResponse) => {
+        if (error.status === 404) {
+          this.snackBar.open('Niepoprawny adres e-mail. Wprowadź poprawny adres','',{duration:3000,panelClass: 'red-snackbar',verticalPosition:'top',horizontalPosition:'center'})
+        } else {
+          console.log(error);
+          this.snackBar.open
+          ('Sprawdź swoją pocztę,został wysłany e-mail z Twoim loginem. ' +
+            ' Możesz poprosić o e-mail z nowym linkiem poprzez\n' +
+            ' wpisując nowy adres w polu poniżej.','',{duration:6000,panelClass: 'green-snackbar',verticalPosition:'top',horizontalPosition:'center'});
+        }
+      });
   }
 
 }
